9de64bc2a12c5b818fdf59cc3f2721a43c1c5edb
Diego Bonilla <dcbcueva@gmail.com>
2022-09-27 13:48:57 -0500

    ci:modificacion de js y html

---
+++ script.js
@@ -0,0 +1,36 @@
+const inputA = document.getElementById("num1");
+const inputB = document.getElementById("num2");
+const inputR = document.getElementById("resp");
+
+const buttonSuma = document.getElementById("suma");
+const buttonRest = document.getElementById("rest");
+const buttonMult = document.getElementById("mult");
+const buttonDivi = document.getElementById("divi");
+
+const suma = (a, b) => a + b;
+const rest = (a, b) => a - b;
+const mult = (a, b) => a * b;
+const divi = (a, b) => a / b;
+
+const operacionSuma = () => {
+    const x = parseInt(inputA.value);
+    const y = parseInt(inputB.value);
+    inputR.value = suma(x,y);
+}
+
+const operacionRest = () => {
+    inputR = rest(inputA.value, inputB.value);
+}
+
+const operacionMult = () => {
+    inputR = mult(inputA.value, inputB.value);
+}
+
+const operacionDivi = () => {
+    inputR = divi(inputA.value, inputB.value);
+}
+
+buttonSuma.addEventListener("click", operacionSuma);
+buttonRest.addEventListener("click", operacionRest);
+buttonMult.addEventListener("click", operacionMult);
+buttonDivi.addEventListener("click", operacionDivi);
